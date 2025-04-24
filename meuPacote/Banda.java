7852653b921f267db3f13ce1dee175859753820d
Eduardo Baroli Souza <eduardo_Baroli@hotmail.com>
2025-04-24 11:53:37 -0300

    ARRUMANDO ERRO DE PORTUGUES :)

--- meuPacote/Banda.java
+++ meuPacote/Banda.java
@@ -7,7 +7,7 @@ public class Banda extends Artista {
     public Banda(String nome, int diaDoShow, int mesDoShow, Genero genero) {
         super(nome, diaDoShow, mesDoShow); // HERANÇA
         this.genero = genero;
-        genero.adicionarBanda(this); // ADICIONANDO GENERO POR COMPOSIÇÃO AQUI A BANDA SE ADICIONA A SI MESMA NO GÊNERO LOGO BANDA POSSUI UM GENERO
+        genero.adicionarBanda(this); // ADICIONANDO GENERO POR COMPOSIÇÃO AQUI A BANDA SE ADICIONA NO GÊNERO LOGO BANDA POSSUI UM GENERO
     }
 
     public Genero getGenero() {
